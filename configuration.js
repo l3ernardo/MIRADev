f01319c7369ec6d22e361379af914611b2ed96a3
Gabriela Soledad Pailiacho Guevara <gsoledad@br.ibm.com>
2016-06-20 15:13:29 -0300

    Update faces url

--- configuration.js
+++ configuration.js
@@ -6,6 +6,6 @@ module.exports = {
 	keyNameBU: 'BusinessUnit',
 	msgIdPassR: 'IBM Intranet ID and/or password is required',
 	msgIdPassW: 'Wrong IBM Intranet ID and/or password.',
-	bpURLcn: 'http://faces.tap.ibm.com/api/find/?q=%t',
-	bpURLmail: 'http://faces.tap.ibm.com/api/find/?q=%t'
+	bpURLcn: 'https://faces.tap.ibm.com/api/find/?q=%t',
+	bpURLmail: 'https://faces.tap.ibm.com/api/find/?q=%t'
 }
