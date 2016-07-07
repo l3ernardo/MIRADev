f854f1d420fee1e65291a3db1156515d3c24ff3d
Gabriela Soledad Pailiacho Guevara <gsoledad@br.ibm.com>
2016-07-07 12:01:48 -0300

    Fix user's multigroup

--- server/config/js/class-accessrules.js
+++ server/config/js/class-accessrules.js
@@ -13,56 +13,41 @@ var accessrules = {
 			Format (example): var fldlist = doc[0].AdditionalReaders + doc[0].Owner + doc[0].Focals;
 			As the email is the key, no need to concern about arrays; append them all as a single string
 		*/
-		var rules = {
-			"editor":false,
-			"admin":false,
-			"grantaccess":false,
-			"resetstatus":false,
-			"cuadmin":false
-		}
 		var usr = "(" + req.session.user.mail + ")";
 		var editor = false;
+		var admin = false;
+		var grantaccess = false;
+		var resetstatus = false;
+		var cuadmin = false;
 		if(fldlist.indexOf(usr) !== -1) {
 			editor = true;
 		}
-		switch(req.session.user.groupName) {
-			case "MIRA-ADMIN":
-				var rules = {
-					"editor":editor,
-					"admin":true,
-					"grantaccess":true,
-					"resetstatus":true,
-					"cuadmin":true
-				}				
-			break;
-			case "MIRA-GRANT-ACCESS":
-				var rules = {
-					"editor":editor,
-					"admin":false,
-					"grantaccess":true,
-					"resetstatus":false,
-					"cuadmin":false
+		var roles = ['MIRA-ADMIN','MIRA-GRANT-ACCESS','MIRA-RESET-STATUS','MIRA-CU-ADMIN-DATA'];
+		for(var i=0;i<roles.length;i++) {
+			if(req.session.user.groupName.indexOf(roles[i])!=-1) {
+				switch (roles[i]) {
+					case 'MIRA-ADMIN':
+					admin = true;
+					break;
+					case 'MIRA-GRANT-ACCESS':
+					grantaccess = true;
+					break;
+					case 'MIRA-RESET-STATUS':
+					resetstatus = true;
+					break;
+					case 'MIRA-CU-ADMIN-DATA':
+					cuadmin = true;
+					break;					
 				}
-				break;
-			case "MIRA-RESET-STATUS":
-				var rules = {
-					"editor":editor,
-					"admin":false,
-					"grantaccess":false,
-					"resetstatus":true,
-					"cuadmin":false
-				}
-			break;
-			case "MIRA-CU-ADMIN-DATA":
-				var rules = {
-					"editor":editor,
-					"admin":false,
-					"grantaccess":false,
-					"resetstatus":false,
-					"cuadmin":true
-				}
-			break;				
+			}
 		}
+		var rules = {
+			"editor":editor,
+			"admin":admin,
+			"grantaccess":grantaccess,
+			"resetstatus":resetstatus,
+			"cuadmin":cuadmin
+		}		
 		this.rules = rules;
 	}			
 };
