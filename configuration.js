a8bfb88f4d253ffe852a79d0c40dfc5505fb92fb
Gabriela Soledad Pailiacho Guevara <gsoledad@br.ibm.com>
2016-07-21 16:44:40 -0300

    Update setup functionality
    Configuration file was modified due to Menu parameter's name change.
    Setup functionality was modified, to not use ajax and reduce queries to
    cloudant. Useless methods were deleted.

--- configuration.js
+++ configuration.js
@@ -2,7 +2,7 @@ module.exports = {
 	siteTitle: "MIRA",
 	conTitle: 'MSAC Integrated Reporting Application',
 	siteDesc: 'The MSAC Web Tool',
-	keyNameM: 'MenuTitle',
+	keyNameM: 'MIRAMenu',
 	keyNameBU: 'BusinessUnit',
 	msgIdPassR: 'IBM Intranet ID and/or password is required',
 	msgIdPassW: 'Wrong IBM Intranet ID and/or password.',
