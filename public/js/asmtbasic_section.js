6a8d9d7734f7eb385b03178bc10fe6a36295040f
Minerva S. GENON <genonms@ph.ibm.com>
2016-08-20 14:30:15 +0800

    added CP asmt basics

---
+++ public/js/asmtbasic_section.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+	if ($("input[name='showOutlook']").val() == 1) {
+		$(".outlook").show();
+	} else {
+		$(".outlook").hide();
+	}
+
+	// if ($("input[name='showOutlook']").val() == 1) {
+	//
+	// 	// IOT name change
+	// 	$("#iotname").change(function(){
+	// 		$("#IOTid").val($(this).val());
+	// 		$("#IOT").val($("#iotname option:selected").text());
+	// 	});
+	//
+	// 	// IMT name change
+	// 	$("#imtname").change(function(){
+	// 		$("#IMTid").val($(this).val());
+	// 		$("#IMT").val($("#imtname option:selected").text());
+	// 	});
+	//
+	// 	// Country name change
+	// 	$("#countryname").change(function(){
+	// 		$("#Countryid").val($(this).val());
+	// 		$("#Country").val($("#countryname option:selected").text());
+	// 	});
+	//
+	// }
+
+});
