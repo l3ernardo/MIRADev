b11e2bf845f1d1d48ed6f1696e0a8f0b12c639c7
Minerva S. GENON <genonms@ph.ibm.com>
2016-09-15 23:26:14 +0800

    added code for CP edit mode

--- public/js/assessment.js
+++ public/js/assessment.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	//Hide left navigation
 	hide_divs();
+	//display notes
+ 	$("#NotesReadOnly").html($("input[name='NotesRO']").val());
 
 	//Code for Edit button
 	$('#btn_edit').click(function() {
@@ -12,13 +14,35 @@ $(document).ready(function(){
 	});
 	//Code for Save button
 	$('#btn_save').click(function(evt) {
+		myEditor.saveHTML();
+		var YmyEditor = myEditor.get('element').value;
+		$('#Notes').val(YmyEditor);
 		$("#assessment").submit();
 	});
 	//Code for Save & Close button
 	$('#btn_save_close').click(function(evt) {
+		myEditor.saveHTML();
+		var YmyEditor = myEditor.get('element').value;
+		$('#Notes').val(YmyEditor);
 		$('#close').val('1');
 		$("#assessment").submit();
 	});
+
+	// --- start of rich text section --- //
+	//Setup some private variables
+	var Dom = YAHOO.util.Dom,
+		Event = YAHOO.util.Event;
+	//The SimpleEditor config
+	var myConfig = {
+		height: '200px',
+		width: '100%',
+		dompath: true,
+		focusAtStart: true
+	};
+	//Load the SimpleEditor
+	myEditor = new YAHOO.widget.SimpleEditor('Notes', myConfig);
+	myEditor.render();
+	// --- end of rich text section --- //
 
 });
 
