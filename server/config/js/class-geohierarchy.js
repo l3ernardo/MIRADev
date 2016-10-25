1fffa7c07de4d943df9d2b87778124b8bdb0ed07
Carlos Ramirez <cramirez@mx1.ibm.com>
2016-10-25 09:50:04 -0500

    GEO Hierarchy with restful on

--- server/config/js/class-geohierarchy.js
+++ server/config/js/class-geohierarchy.js
@@ -11,102 +11,61 @@ var q = require("q");
 var parameter = require('./class-parameter.js');
 var util = require('./class-utility.js');
 
-
 var geohierarchy = {
-	getGeoHierarchy: function (req) { 
-		var result = "";
-		//var deferred = q.defer();
-		try  {
-			if (req.session.businessunit == "GBS") {
-				result = "GeoHierarchy -- GBS";
-				console.log("GeoHierarchy -- GBS");
-				return result;
-			}
-			else {
-				result = "GeoHierarchy -- GTS";
-				console.log("GeoHierarchy -- GTS");
-				return result;
-			}
-		}
-		catch(e){
-			//deferred.reject({"status": 500, "error": e});
-		}
-	},
-	
-	createGEOHierarchy: function(req,db){
+
+createGEOHierarchy: function(req){
 			var deferred = q.defer();
 
 			var IOT = [];
 			var IMT = {};
+			var key = {};
 			var countries = [];
 			var country = {};
 			var iot = {};
 			var indexIOT = {};
 			var indexIOTIMTs = {};
 			var response = {};
-			var uri = "https://eapim-dev.w3ibm.mybluemix.net/portfoliomgmt/development/mira/hierarchy?QTR=";
-	
-
-			req.query.keyName = "ImportWWBCITData";
-
+			var uri = "http://mira-connector-dev.w3ibm.mybluemix.net/showAlldata?designdoc=wwbcitdocs&viewname=hierarchy";
 			try{
-	
-				parameter.getParam(req,db).then(function(data){
-
-					 uri = uri+"'"+data.doc.value.quarter+"'&format=j";
+				util.callhttp(uri).then(function(data){
 					
-
-				try{
-					util.callhttp(uri).then(function(data){
-					
-						var json = data.doc.response.resultset;
-						
+						var json = data.doc;
 					
 				try{
 					for(var i=0;i<json.length;i++){  //Iterate on all the response by country
-				
-						
-							
-
-								 country["name"] = json[i].row.COUNTRY;
-								 country["IMT"] = json[i].row.SUB_GEO;
-								 country["IOT"] = json[i].row.GEO;
+								 country["id"] = json[i].ID;
+								 country["name"] = json[i].COUNTRY;
+								 country["IMT"] = json[i].SUB_GEO;
+								 country["IOT"] = json[i].GEO;
+								 
 								 countries.push(country);
 								 
-								 if(typeof IMT[json[i].row.SUB_GEO] !== 'undefined'){ // check if IMT exist and add the addional countries
-										
-								 	
-				
-									IMT[json[i].row.SUB_GEO].push(json[i].row.COUNTRY);
+								 if(typeof IMT[json[i].SUB_GEO] !== 'undefined'){ // check if IMT exist and add the addional countries
+									IMT[json[i].SUB_GEO].push(json[i].COUNTRY);
 
 								 }else{  // if not add the new IMT with its country
 							
-									IMT[json[i].row.SUB_GEO] = [json[i].row.COUNTRY];
-									
-
+									IMT[json[i].SUB_GEO] = [json[i].COUNTRY];
+							
 									imt= {};
 								}
 
-								if(typeof indexIOT[json[i].row.GEO] !== 'undefined' ){ // check if IOT exist and add the addional countries
-									if(typeof indexIOTIMTs[json[i].row.SUB_GEO] === 'undefined' ){
-										IOT[indexIOT[json[i].row.GEO]].IMTs.push(json[i].row.SUB_GEO);
-										indexIOTIMTs[json[i].row.SUB_GEO] = "true";
+								if(typeof indexIOT[json[i].GEO] !== 'undefined' ){ // check if IOT exist and add the addional countries
+									if(typeof indexIOTIMTs[json[i].SUB_GEO] === 'undefined' ){
+										IOT[indexIOT[json[i].GEO]].IMTs.push(json[i].SUB_GEO);
+										indexIOTIMTs[json[i].SUB_GEO] = "true";
 									}
-										
-	
+							
 								 }else{  // if not add the new IMT with its country
-									iot["name"] = json[i].row.GEO;
-									iot["IMTs"] = [json[i].row.SUB_GEO];
+									iot["name"] = json[i].GEO;
+									iot["IMTs"] = [json[i].SUB_GEO];
 									IOT.push(iot);	
-									indexIOT[json[i].row.GEO] = IOT.length-1;
-									indexIOTIMTs[json[i].row.SUB_GEO] = "true";
+									indexIOT[json[i].GEO] = IOT.length-1;
+									indexIOTIMTs[json[i].SUB_GEO] = "true";
 									iot= {};
 								}
-					
-								 
+					 
 								 country = {};
-
-						
 	
 					}
 					}catch(e){console.log(e);}
@@ -129,26 +88,10 @@ var geohierarchy = {
 					deferred.reject({"status": 500, "error": e});
 				}			
 			
-
-		
-
-			}).catch(function(err) {//end callhttp
-					console.log("[routes][geohierarchy] - " + err);
-					deferred.reject({"status": 500, "error": err.error.reason});
-				});
-
-
-		}catch(e){
-			deferred.reject({"status": 500, "error": e});
-			}
-
-
-
-			return deferred.promise;
+	return deferred.promise;
 
 		
 		},
-
 
 }
 
