c9760452f4201087ec5472ca64861d5d2d122ee7
Minerva S. GENON <genonms@ph.ibm.com>
2016-12-13 21:32:54 +0800

    added code for GTS CRM & SOD metrics

--- server/config/js/class-opmetric.js
+++ server/config/js/class-opmetric.js
@@ -87,42 +87,51 @@ var calculatefield = {
 
       if (doc[0].ParentDocSubType == "Country Process" || doc[0].ParentDocSubType == "Controllable Unit" || doc[0].ParentDocSubType == "Account" ) {
         // For Base Level Assessments
-        for (var j = 0; j < dataParam.parameters[opMetricKey][0].options.length; ++j) {
-          if (doc[0].OpMetricKey == dataParam.parameters[opMetricKey][0].options[j].id ) {
-            for (var k = 0; k < dataParam.parameters[opMetricKey][0].options[j].metrics.length; ++k) {
-              opID = dataParam.parameters[opMetricKey][0].options[j].metrics[k];
-              if (opMetricIDs == "") opMetricIDs = opID;
-              else opMetricIDs = opMetricIDs + "," + opID;
-              omIndex = util.getIndex(dataParam.parameters[paramOpMetrics][0].options,"id",opID)
-              doc[0].OpMetric.push({"id": opID});
-              doc[0].OpMetric[k].name = dataParam.parameters[paramOpMetrics][0].options[omIndex].name;
-              if (dataParam.parameters[paramOpMetrics][0].options[omIndex].desc) {
-                doc[0].OpMetric[k].desc = dataParam.parameters[paramOpMetrics][0].options[omIndex].desc.split("<br />");
-              }
-              doc[0].OpMetric[k].namefield = opID + "Name";
-              doc[0].OpMetric[k].ratingfield = opID + "Rating";
-              doc[0].OpMetric[k].targetsatdatefield = opID + "TargetSatDate";
-              doc[0].OpMetric[k].colDate = "colDate"+ opID;
-              doc[0].OpMetric[k].findingfield = opID + "Finding";
-              doc[0].OpMetric[k].colFinding = "colFinding"+ opID;
-              doc[0].OpMetric[k].actionfield = opID + "Action";
-              doc[0].OpMetric[k].colFinding = "colAction"+ opID;
-              doc[0].OpMetric[k].rating = "";
-              doc[0].OpMetric[k].targetsatdate = "";
-              doc[0].OpMetric[k].finding = "";
-              doc[0].OpMetric[k].action = "";
-              if (doc[0].OpMetricCurr) {
-                omIndex = util.getIndex(doc[0].OpMetricCurr,"id",opID);
-                if (omIndex != -1) {
-                  doc[0].OpMetric[k].rating = doc[0].OpMetricCurr[omIndex].rating;
-                  doc[0].OpMetric[k].targetsatdate = doc[0].OpMetricCurr[omIndex].targetsatdate;
-                  doc[0].OpMetric[k].finding = doc[0].OpMetricCurr[omIndex].finding;
-                  doc[0].OpMetric[k].action = doc[0].OpMetricCurr[omIndex].action;
-                  if (doc[0].OpMetric[k].rating == "Marg" || doc[0].OpMetric[k].rating == "Unsat") doc[0].opMetricException = 1;
+        if (req.session.businessunit == "GTS" && doc[0].OpMetricKey == "OMKID4") {
+          // OMKID4 is opmetric key id for "Delivery"
+          if (doc[0].OtherMetricRating == "Marg" || doc[0].OtherMetricRating == "Unsat") {
+            doc[0].opMetricException = 1;
+          } else {
+            doc[0].opMetricException = 0;
+          }
+        } else {
+          for (var j = 0; j < dataParam.parameters[opMetricKey][0].options.length; ++j) {
+            if (doc[0].OpMetricKey == dataParam.parameters[opMetricKey][0].options[j].id ) {
+              for (var k = 0; k < dataParam.parameters[opMetricKey][0].options[j].metrics.length; ++k) {
+                opID = dataParam.parameters[opMetricKey][0].options[j].metrics[k];
+                if (opMetricIDs == "") opMetricIDs = opID;
+                else opMetricIDs = opMetricIDs + "," + opID;
+                omIndex = util.getIndex(dataParam.parameters[paramOpMetrics][0].options,"id",opID)
+                doc[0].OpMetric.push({"id": opID});
+                doc[0].OpMetric[k].name = dataParam.parameters[paramOpMetrics][0].options[omIndex].name;
+                if (dataParam.parameters[paramOpMetrics][0].options[omIndex].desc) {
+                  doc[0].OpMetric[k].desc = dataParam.parameters[paramOpMetrics][0].options[omIndex].desc.split("<br />");
+                }
+                doc[0].OpMetric[k].namefield = opID + "Name";
+                doc[0].OpMetric[k].ratingfield = opID + "Rating";
+                doc[0].OpMetric[k].targetsatdatefield = opID + "TargetSatDate";
+                doc[0].OpMetric[k].colDate = "colDate"+ opID;
+                doc[0].OpMetric[k].findingfield = opID + "Finding";
+                doc[0].OpMetric[k].colFinding = "colFinding"+ opID;
+                doc[0].OpMetric[k].actionfield = opID + "Action";
+                doc[0].OpMetric[k].colFinding = "colAction"+ opID;
+                doc[0].OpMetric[k].rating = "";
+                doc[0].OpMetric[k].targetsatdate = "";
+                doc[0].OpMetric[k].finding = "";
+                doc[0].OpMetric[k].action = "";
+                if (doc[0].OpMetricCurr) {
+                  omIndex = util.getIndex(doc[0].OpMetricCurr,"id",opID);
+                  if (omIndex != -1) {
+                    doc[0].OpMetric[k].rating = doc[0].OpMetricCurr[omIndex].rating;
+                    doc[0].OpMetric[k].targetsatdate = doc[0].OpMetricCurr[omIndex].targetsatdate;
+                    doc[0].OpMetric[k].finding = doc[0].OpMetricCurr[omIndex].finding;
+                    doc[0].OpMetric[k].action = doc[0].OpMetricCurr[omIndex].action;
+                    if (doc[0].OpMetric[k].rating == "Marg" || doc[0].OpMetric[k].rating == "Unsat") doc[0].opMetricException = 1;
+                  }
                 }
               }
+              break;
             }
-            break;
           }
         }
       } else {
