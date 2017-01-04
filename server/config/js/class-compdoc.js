f4c09d91ae5ae83a2c3bdb5aefd5ba0b54c83255
Minerva S. GENON <genonms@ph.ibm.com>
2017-01-05 01:30:37 +0800

    updated CP asmt
    - fixed issue on GTS key controls tab, data not loading
    - updated codes using req.app.businessunit based on Carlos K's comment
    - created functions for component doc codes

--- server/config/js/class-compdoc.js
+++ server/config/js/class-compdoc.js
@@ -16,73 +16,90 @@ var getDocs = {
     try {
       switch (doc[0].ParentDocSubType) {
         case "Country Process":
-        var compObj = {
-          selector : {
-            "_id": {"$gt":0},
-            "docType": "asmtComponent",
-            "reportingQuarter": doc[0].CurrentPeriod,
-            "$or": [
-              { "$and": [{"compntType": "countryControls"}] },
-              { "$and": [{"compntType": "controlSample"}] },
-              { "$and": [{"compntType": "sampledCountry"}] }
-            ]
-          }
-        };
-        // { "$and": [{"compntType": "countryControls"}, {"ParentWWBCITKey": doc[0].WWBCITKey}, {"status": {"$ne": "Retired"}}] },
-        // { "$and": [{"compntType": "controlSample"}, {"reportingCountry": doc[0].Country}, {"processSampled": doc[0].GlobalProcess}, {"status": {"$ne": "Retired"}}] },
-        // { "$and": [{"compntType": "sampledCountry"}, {"CPParentIntegrationKeyWWBCIT": doc[0].WWBCITKey}, {"status": {"$ne": "Retired"}}] }
+          var compObj = {
+            selector : {
+              "_id": {"$gt":0},
+              "docType": "asmtComponent",
+              "$or": [
+                // Key Controls Testing Tab
+                { "$and": [{"compntType": "countryControls"},{"reportingQuarter": doc[0].CurrentPeriod},{"owningBusinessUnit": doc[0].BusinessUnit}] },
+                { "$and": [{"compntType": "controlSample"},{"reportingQuarter": doc[0].CurrentPeriod},{"owningBusinessUnit": doc[0].BusinessUnit}] },
+                { "$and": [{"compntType": "sampledCountry"},{"reportingQuarter": doc[0].CurrentPeriod},{"owningBusinessUnit": doc[0].BusinessUnit}] },
+                // Audits and Reviews Tab
+                { "$and": [{"compntType": "ppr"},{"RPTG_BUSINESS_UNIT": doc[0].BusinessUnit},{"CPWWBCITKey" : doc[0].WWBCITKey}] },
+                { "$and": [{"compntType": "internalAudit"},{"RPTG_BUSINESS_UNIT": doc[0].BusinessUnit},{"CPWWBCITKey" : doc[0].WWBCITKey}] },
+                { "$and": [{"compntType": "ppr"},{"RPTG_BUSINESS_UNIT": doc[0].BusinessUnit},{"CPWWBCITKey" : doc[0].WWBCITKey},{"REVIEW_TYPE": "CHQ Internal Audit"}] },
+                { "$and": [{"DOCTYPE": "ppreview"},{"RPTG_BUSINESS_UNIT": doc[0].BusinessUnit}] }
+              ]
+            }
+          };
+          // { "$and": [{"compntType": "countryControls"}, {"ParentWWBCITKey": doc[0].WWBCITKey}, {"status": {"$ne": "Retired"}}] },
+          // { "$and": [{"compntType": "controlSample"}, {"reportingCountry": doc[0].Country}, {"processSampled": doc[0].GlobalProcess}, {"status": {"$ne": "Retired"}}] },
+          // { "$and": [{"compntType": "sampledCountry"}, {"CPParentIntegrationKeyWWBCIT": doc[0].WWBCITKey}, {"status": {"$ne": "Retired"}}] }
 
-        db.find(compObj).then(function(compdata) {
-          var comps = compdata.body.docs;
-          doc[0].risks = [];
-          doc[0].RCTestData = [];
-          doc[0].SCTestData = [];
-          doc[0].SampleData = [];
-          var controlCtr = 0;
-          var scControlCtr = 0;
-          var sampleCtr = 0;
-          for(var i = 0; i < comps.length; i++) {
-            if (comps[i].compntType == "openIssue") {
-              doc[0].risks.push(comps[i]);
-            }
-            else if (comps[i].compntType == "countryControls") {
-              doc[0].RCTestData.push(comps[i]);
-              // Calculate for Defect Rate of Control doc
-              if (doc[0].RCTestData[controlCtr].numActualTests ==  undefined || doc[0].RCTestData[controlCtr].numActualTests == "" || doc[0].RCTestData[controlCtr].numActualTests == 0 || doc[0].RCTestData[controlCtr].numDefects == undefined || doc[0].RCTestData[controlCtr].numDefects == "") {
-                doc[0].RCTestData[controlCtr].defectRate = "";
-              } else {
-                doc[0].RCTestData[controlCtr].defectRate = ((doc[0].RCTestData[controlCtr].numDefects/doc[0].RCTestData[controlCtr].numActualTests) * 100).toFixed(1);
+          db.find(compObj).then(function(compdata) {
+            var comps = compdata.body.docs;
+            doc[0].risks = [];
+            doc[0].RCTestData = [];
+            doc[0].SCTestData = [];
+            doc[0].SampleData = [];
+            doc[0].AuditTrustedData = [];
+            doc[0].AuditTrustedRCUData = [];
+            doc[0].AuditLocalData = [];
+            var controlCtr = 0;
+            var scControlCtr = 0;
+            var sampleCtr = 0;
+            for(var i = 0; i < comps.length; i++) {
+              if (comps[i].compntType == "openIssue") {
+                doc[0].risks.push(comps[i]);
               }
-              // Calculate for ControlName
-              doc[0].RCTestData[controlCtr].controlName = doc[0].RCTestData[controlCtr].controlReferenceNumber.split("-")[2] + " - " + doc[0].RCTestData[controlCtr].controlShortName;
-              controlCtr++;
-            }
-            else if (comps[i].compntType == "sampledCountry") {
-              doc[0].SCTestData.push(comps[i]);
-              // Calculate for ControlName
-              doc[0].SCTestData[scControlCtr].controlName = doc[0].SCTestData[scControlCtr].controlReferenceNumber.split("-")[2] + " - " + doc[0].SCTestData[scControlCtr].controlShortName;
-              scControlCtr++;
-            }
-            else if (comps[i].compntType == "controlSample") {
-              doc[0].SampleData.push(comps[i]);
-              if (comps[i].controlType == "KCO") {
-                doc[0].SampleData[sampleCtr].processCategory = "Operational";
-              } else {
-                doc[0].SampleData[sampleCtr].processCategory = "Financial";
+              else if (comps[i].compntType == "countryControls") {
+                doc[0].RCTestData.push(comps[i]);
+                // Calculate for Defect Rate of Control doc
+                if (doc[0].RCTestData[controlCtr].numActualTests ==  undefined || doc[0].RCTestData[controlCtr].numActualTests == "" || doc[0].RCTestData[controlCtr].numActualTests == 0 || doc[0].RCTestData[controlCtr].numDefects == undefined || doc[0].RCTestData[controlCtr].numDefects == "") {
+                  doc[0].RCTestData[controlCtr].defectRate = "";
+                } else {
+                  doc[0].RCTestData[controlCtr].defectRate = ((doc[0].RCTestData[controlCtr].numDefects/doc[0].RCTestData[controlCtr].numActualTests) * 100).toFixed(1);
+                }
+                // Calculate for ControlName
+                doc[0].RCTestData[controlCtr].controlName = doc[0].RCTestData[controlCtr].controlReferenceNumber.split("-")[2] + " - " + doc[0].RCTestData[controlCtr].controlShortName;
+                controlCtr++;
               }
-              // Calculate for ControlName
-              doc[0].SampleData[sampleCtr].controlName = doc[0].SampleData[sampleCtr].controlReferenceNumber.split("-")[2] + " - " + doc[0].SampleData[sampleCtr].controlShortName;
-              sampleCtr++;
-            }
-            else {
+              else if (comps[i].compntType == "sampledCountry") {
+                doc[0].SCTestData.push(comps[i]);
+                // Calculate for ControlName
+                doc[0].SCTestData[scControlCtr].controlName = doc[0].SCTestData[scControlCtr].controlReferenceNumber.split("-")[2] + " - " + doc[0].SCTestData[scControlCtr].controlShortName;
+                scControlCtr++;
+              }
+              else if (comps[i].compntType == "controlSample") {
+                doc[0].SampleData.push(comps[i]);
+                if (comps[i].controlType == "KCO") {
+                  doc[0].SampleData[sampleCtr].processCategory = "Operational";
+                } else {
+                  doc[0].SampleData[sampleCtr].processCategory = "Financial";
+                }
+                // Calculate for ControlName
+                doc[0].SampleData[sampleCtr].controlName = doc[0].SampleData[sampleCtr].controlReferenceNumber.split("-")[2] + " - " + doc[0].SampleData[sampleCtr].controlShortName;
+                sampleCtr++;
+              }
+              else if (comps[i].compntType == "ppr" && comps[i].REVIEW_TYPE == "CHQ Internal Audit") {
+                doc[0].AuditTrustedRCUData.push(comps[i]);
+              }
+              else if (comps[i].compntType == "ppr" || comps[i].compntType == "internalAudit") {
+                doc[0].AuditTrustedData.push(comps[i]);
+              }
+              else if (comps[i].DOCTYPE == "ppreview") {
+                doc[0].AuditLocalData.push(comps[i]);
+              }
+              else {
 
+              }
             }
-          }
-          deferred.resolve({"status": 200, "doc": doc});
-        }).catch(function(err) {
-          console.log("[class-compdoc][getCompDocs] - " + err.error);
-          deferred.reject({"status": 500, "error": err.error.reason});
-        });
+            deferred.resolve({"status": 200, "doc": doc});
+          }).catch(function(err) {
+            console.log("[class-compdoc][getCompDocs] - " + err.error);
+            deferred.reject({"status": 500, "error": err.error.reason});
+          });
         break;
         case "Global Process":
         break;
