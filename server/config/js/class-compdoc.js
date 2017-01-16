87ef298fefaeea2377bb2a9a2ca12518848b1b0d
Artur Bernardo Queiroz Da Silva <bqueiroz@br.ibm.com>
2017-01-16 08:55:18 -0300

    Update class-compdoc.js

--- server/config/js/class-compdoc.js
+++ server/config/js/class-compdoc.js
@@ -179,7 +179,7 @@ var getDocs = {
                 { "$and": [{"compntType": "CUSummarySample"},{"reportingQuarter": doc[0].CurrentPeriod},{"controllableUnit": doc[0].AssessableUnitName}] },
                 { "$and": [{"compntType": "controlSample"},{"reportingQuarter": doc[0].CurrentPeriod},{"controllableUnit": doc[0].AssessableUnitName}] },
                 // Audits and Reviews Tab
-                { "$and": [{"compntType": "PPR"},{"CU" : doc[0].AssessableUnitName}] },
+                { "$and": [{"compntType": "PPR"},{"AssessableUnitName" : doc[0].AssessableUnitName}] },
                 { "$and": [{"compntType": "internalAudit"},{"parentid":doc[0].parentid}] },
                 { "$and": [{"compntType": "localAudit"},{"parentid": doc[0]._id}] }
               ]
